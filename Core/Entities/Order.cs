cbc9162d70332b6f8ff359425e855273a86d809c
alexscespedes <alexandersenc99@gmail.com>
2025-06-13 18:57:47 -0400

    Update order entity class

--- Core/Entities/Order.cs
+++ Core/Entities/Order.cs
@@ -6,17 +6,17 @@ public class Order
     public int OrderId { get; set; }
     public Customer Customer { get; set; }
     public Product Product { get; set; }
-    public List<int> Quantity { get; set; }
+    public int Quantity { get; set; }
     public DateTime OrderDate { get; set; }
-    public List<decimal> TotalAmount { get; set; }
-    public Order(Customer customer, Product product, List<int> quantity, DateTime orderDate, List<decimal> totalAmount)
+    public decimal TotalAmount { get; set; }
+    public Order(Customer customer, Product product, int quantity, List<decimal> totalAmount)
     {
         OrderId = Interlocked.Increment(ref nextId);
         Customer = customer;
         Product = product;
         Quantity = quantity;
-        OrderDate = orderDate;
-        TotalAmount = totalAmount;
+        OrderDate = DateTime.Now;
+        TotalAmount = Quantity * Product.Price;
     }
 
 }
